09d9ea94e87387a2fde711337d690ed326291719
Juan Camilo Jiménez <camilocjuan@gmail.com>
2021-10-25 22:58:55 -0500

    esto es todo

--- src/nucasim/Nucasim.java
+++ src/nucasim/Nucasim.java
@@ -7,6 +7,7 @@ public class Nucasim {
     public static void main(String[] args) {
         System.out.println("hola este es el primer commitls");
         System.out.println("hola");
+        System.out.println("kkkk");
     }
     
 }
