30b20bf868525280289ff95fecef04936dfa2f6f
Juan Camilo Jiménez <camilocjuan@gmail.com>
2021-10-25 22:49:03 -0500

    Repositorio nucasim

--- src/nucasim/Nucasim.java
+++ src/nucasim/Nucasim.java
@@ -6,6 +6,7 @@ public class Nucasim {
 
     public static void main(String[] args) {
         System.out.println("hola este es el primer commitls");
+        System.out.println("hola");
     }
     
 }
