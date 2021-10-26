c5902f0d24b1e604e63fe02e4837468e09566afe
Juan Camilo Jiménez <camilocjuan@gmail.com>
2021-10-26 12:36:14 -0500

    nuevo cambio de prueba

--- src/nucasim/Nucasim.java
+++ src/nucasim/Nucasim.java
@@ -6,7 +6,7 @@ public class Nucasim {
 
     public static void main(String[] args) {
         System.out.println("hola este es el primer commitls");
-        System.out.println("hola");
+        System.out.println("hola este es un nuevo cambio ");
         System.out.println("kkkk");
     }
     
