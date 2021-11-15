14203d8ad125e50b43158283f8b03100ea1aa3ce
Juan Camilo Jiménez <camilocjuan@gmail.com>
2021-11-15 18:30:27 -0500

    prueba de ssh

--- src/nucasim/Nucasim.java
+++ src/nucasim/Nucasim.java
@@ -5,6 +5,7 @@ public class Nucasim {
 
     public static void main(String[] args) {
         system.out.printl("Hola yuli este es el repositorio para empezar a trabajar remotamente ya esta configurado y listo")
+        system.out.printlnl("jejeje ese ssh cambio")
     }
     
 }
